9524c6af5a3fa3aa33996a4017f39375697aa4e1
Artsiom_Barodka <artsiombarodka@gmail.com>
2023-05-29 13:57:28 +0300

    added task 1

---
+++ core-js-part1/task1.js
@@ -0,0 +1,10 @@
+function getSum(number1, number2) {
+  return number1 + number2;
+}
+
+console.log(`Sum of 1 and 1 is ${getSum(1, 1)}`);
+console.log(`Sum of 1000 and 1000 is ${getSum(1000, 1000)}`);
+console.log(`Sum of 99999 and 555555555 is ${getSum(99999, 555555555)}`);
+console.log(
+  `Sum of 9999999999 and 9999999999999 is ${getSum(9999999999, 9999999999999)}`
+);
