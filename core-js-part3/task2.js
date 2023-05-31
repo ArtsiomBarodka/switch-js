fc767caf45f427c3e327bb9ad8ff60c35f6b0fa1
Artsiom_Barodka <artsiombarodka@gmail.com>
2023-05-31 17:21:07 +0300

    added task 2

---
+++ core-js-part3/task2.js
@@ -0,0 +1,8 @@
+function getArrayLength(arr) {
+  return arr.length;
+}
+
+console.log(
+  `The length of array [${[1, 2, 4, 6]}] is ${getArrayLength([1, 2, 4, 6])}`
+);
+console.log(`The length of array [${[]}] is ${getArrayLength([])}`);
